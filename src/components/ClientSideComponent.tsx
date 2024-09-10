dc907a4438f546e68712ec877834da94ed864d28
shoken3207 <shoken4797@gmail.com>
2024-09-10 14:34:08 +0900

    trpc mutate 実行

--- src/components/ClientSideComponent.tsx
+++ src/components/ClientSideComponent.tsx
@@ -5,10 +5,16 @@ import React from "react";
 
 const ClientSideComponent = () => {
   const { data } = trpc.hello.useQuery();
+  const addUser = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const msg = await trpc.createUser({});
+  };
   return (
     <div>
       <h1>Client Side Component</h1>
       <div>{JSON.stringify(data)}</div>
+      <button onClick={(e) => addUser(e)}>ユーザー追加</button>
     </div>
   );
 };
